0f4576365d136ccd8774fe65361173403891bb5b
iqDF <daniel.thekurniadi@gmail.com>
2020-01-06 22:23:38 +0800

    [AWS Rekognition] 02 face analysis demo add comments - add useful comments - add help in argparser

--- AWS/rekognition/02.rekognition_face_analysis.py
+++ AWS/rekognition/02.rekognition_face_analysis.py
@@ -6,8 +6,9 @@ import boto3
 def main(args):
     rekognition = boto3.client('rekognition')
 
+    # Configure argument for rekognition.Image
     if args.s3_bucket and args.filepath:
-        # configure argument for rekognition.Image
+        # case: specify API input using image in AWS S3
         img_kwarg = {
             'S3Object': {
                 'Bucket': args.s3_bucket,
@@ -16,6 +17,7 @@ def main(args):
         }
 
     elif args.filepath:
+        # case: uploading image from local machine to rekognition API
         if os.path.isfile(args.filepath):
             # using image file
             with open(args.filepath, 'rb') as f:
@@ -23,15 +25,18 @@ def main(args):
 
             # configure argument for rekognition.Image    
             img_kwarg = {'Bytes': photo_bytes}
-        
+
         else:
+            # error when your local image path is wrong
             raise FileExistsError('File not found in local: %s' % args.filepath)
-    
+
+    # Call Rekognition: detect_labels API
     response = rekognition.detect_faces(
         Image=img_kwarg,
         Attributes=[args.face_attr.upper()]
     )
 
+    # Printing HTTP Response and its data from API call
     pp = pprint.PrettyPrinter(indent=2)
     pp.pprint(response)
 
@@ -40,9 +45,9 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script to try rekognition:detect_label()')
-    parser.add_argument('--filepath', type=str)
-    parser.add_argument('--s3-bucket', type=str)
-    parser.add_argument('--face-attr', type=str, default='DEFAULT')
+    parser.add_argument('--file-path', type=str, help='File path to Amazon S3 or your local file path')
+    parser.add_argument('--s3-bucket', type=str, help='If specified, the API will search `file-path` as a file path in Amazon S3 directory instead')
+    parser.add_argument('--face-attr', type=str, default='DEFAULT', help='Face attributes to be specified as input to Rekognition API.')
 
     args = parser.parse_args()
 
