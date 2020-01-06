4054ca2091fe1c70453e7d0ac3a6855fbe654b9b
iqDF <daniel.thekurniadi@gmail.com>
2020-01-06 22:23:48 +0800

    [AWS Rekognition] 03.face_compare  demo add comments - add useful comments - add help in argparser

--- AWS/rekognition/03.rekognition_face_compare.py
+++ AWS/rekognition/03.rekognition_face_compare.py
@@ -7,10 +7,11 @@ def main(args):
     rekognition = boto3.client('rekognition')
 
     if not args.source_img or not args.target_img:
-        raise Exception('Missing argument(s): sourceimg or targetimg.')
+        raise Exception('Missing argument(s): `source-img` or `target-img`.')
 
+    # Configure argument for rekognition.Image
     if args.s3_bucket:
-        # use images from s3 bucket when specified
+        # case: specify API input using image in AWS S3
         source_img_kwargs = {
             'S3Object': {
                 'Bucket': args.s3_bucket,
@@ -25,21 +26,25 @@ def main(args):
         }
 
     else:
-        # use images from local
+        # case: using both (source and target) images from local machine
         with open(args.source_img, 'rb') as f:
+            # upload source image
             source_img = f.read()
         
         with open(args.target_img, 'rb') as f:
+            # upload target image
             target_img = f.read()
         
         source_img_kwargs = {'Bytes': source_img}
         target_img_kwargs = {'Bytes': target_img}
 
+    # Call Rekognition: detect_labels API
     response = rekognition.compare_faces(
         SourceImage=source_img_kwargs,
         TargetImage=target_img_kwargs
     )
 
+    # Printing HTTP Response and its data from API call
     pp = pprint.PrettyPrinter(indent=2)
     pp.pprint(response)
 
@@ -48,9 +53,12 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script to try rekognition:detect_label()')
-    parser.add_argument('--source-img', type=str, required=True)
-    parser.add_argument('--target-img', type=str, required=True)
-    parser.add_argument('--s3-bucket', type=str, default=None)
+    parser.add_argument('--source-img', type=str, required=True, 
+        help='Source image path to Amazon S3 or your local file path')
+    parser.add_argument('--target-img', type=str, required=True, 
+        help='File path to Amazon S3 or your local file path')
+    parser.add_argument('--s3-bucket', type=str, default=None, 
+        help='If specified, the API will search `file-path` as a file path in Amazon S3 directory instead')
 
     args = parser.parse_args()
 
